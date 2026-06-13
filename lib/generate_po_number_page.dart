a904c749c4973fa959c23971e2dee21374f6ee74
Ritika Sorot <ritika.sorot@gmail.com>
2026-06-13 15:52:23 +0530

    generate so page error resolved

--- lib/generate_po_number_page.dart
+++ lib/generate_po_number_page.dart
@@ -30,19 +30,56 @@ Future<List<String>> getProductManagers() async {
   }
 }
 
+// Future<List<String>> getItems() async {
+//   final response = await http.get(Uri.parse('$apiBaseUrl/get_items'));
+//   print(response.body);
+//   if (response.statusCode == 200) {
+//     return List<String>.from(json.decode(response.body));
+//   } else {
+//     throw Exception('Failed to load items');
+//   }
+// }
+
 Future<List<String>> getItems() async {
   final response = await http.get(Uri.parse('$apiBaseUrl/get_items'));
+
   if (response.statusCode == 200) {
-    return List<String>.from(json.decode(response.body));
+    final List<dynamic> data = json.decode(response.body);
+
+    return data
+        .map((item) => item['name'].toString())
+        .toList();
   } else {
     throw Exception('Failed to load items');
   }
 }
 
+// Future<List<String>> getPurchaseVendors() async {
+//   final response = await http.get(Uri.parse('$apiBaseUrl/get_purchase_vendors'));
+
+
+//   print("VENDORS RESPONSE:");
+//   print(response.body);
+
+
+//   if (response.statusCode == 200) {
+//     return List<String>.from(json.decode(response.body));
+//   } else {
+//     throw Exception('Failed to load purchase vendors');
+//   }
+// }
+
+
 Future<List<String>> getPurchaseVendors() async {
-  final response = await http.get(Uri.parse('$apiBaseUrl/get_purchase_vendors'));
+  final response =
+      await http.get(Uri.parse('$apiBaseUrl/get_purchase_vendors'));
+
   if (response.statusCode == 200) {
-    return List<String>.from(json.decode(response.body));
+    final List<dynamic> data = json.decode(response.body);
+
+    return data
+        .map<String>((vendor) => vendor['name'].toString())
+        .toList();
   } else {
     throw Exception('Failed to load purchase vendors');
   }
